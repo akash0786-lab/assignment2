4a49b6b4fdb6341298bec2d85df88a157e92e8ec
Akash <akasharora375@gmail.com>
2022-10-30 23:47:48 -0400

    updated version

--- server/config/app.js
+++ server/config/app.js
@@ -8,9 +8,7 @@ var logger = require('morgan');
 let session = require('express-session');
 let passport = require('passport');
 
-// let passportJWT = require('passport-jwt');
-// let JWTStrategy = passportJWT.Strategy;
-// let ExtractJWT = passportJWT.ExtractJwt;
+
 
 let passportLocal = require('passport-local');
 let localStrategy = passportLocal.Strategy;
@@ -88,29 +86,6 @@ passport.use(User.createStrategy());
 // serialize and deserialize the User info
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-// let jwtOptions = {};
-// jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
-// jwtOptions.secretOrKey = DB.Secret;
-
-// let strategy = new JWTStrategy(jwtOptions, (jwt_payload, done) => {
-//   User.findById(jwt_payload.id)
-//     .then(user => {
-//       return done(null, user);
-//     })
-//     .catch(err => {
-//       return done(err, false);
-//     });
-// });
-
-// passport.use(strategy);
-
-
-
-
-
-
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
